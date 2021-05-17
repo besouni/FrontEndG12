a596f3316c61903aa84fd065158176fc71b8abca
Beso Tabatadze <besotabatadze84@gmail.com>
2021-05-17 11:59:48 +0400

    Lecture 10

---
+++ Week10/app.js
@@ -0,0 +1,49 @@
+function f(){
+   for(let i=0; i<5; i++){
+      console.log(i);
+   }
+   // console.log("i="+i)
+}
+
+f();
+
+var x = 10;
+t = 34;
+console.log("=========")
+function f1(){
+   var x = 78
+   t = 56
+   if(10>3)
+   {
+      var x = 23;
+   }
+   console.log(x);
+}
+
+f1();
+console.log(x);
+console.log(t);
+console.log("=========")
+
+var y = 45
+let y1 = 67
+console.log(window.y)
+// console.log(window.y1)
+
+let z = 34;
+z = 89
+// var z = 78;
+console.log(z)
+
+const p = 3.14
+console.log(p)
+// p = 98
+// console.log(p)
+
+const st1 = {name:"Keti", lastname:'Zurashvili'}
+console.log(st1)
+// st1 = {name:"Leri", lastname:'kakashvili'}
+st1.name = "Leri"
+st1.lastname = "kakashvili"
+console.log(st1)
+
---
+++ Week10/classwork/app.js
@@ -0,0 +1,37 @@
+m1 = [3, 4, 5, 9]
+console.log(m1)
+
+m2 = [
+      [3, 4, 5, 6, 9, 9],
+      [4, 5, 6, 6],
+      [4, 5, 5],
+      [4, 5, 6, 6, 9],
+]
+
+m3 = []
+
+for(i=0; i<10; i++){
+   m4 = [];
+   for(j=0; j<10; j++){
+      m4[j] = Math.random();
+   }
+   m3[i] = m4;
+}
+
+console.log(m3);
+
+console.log(m2)
+
+tb = "<table border=1>";
+for(i=0; i<m2.length; i++){
+   tb += "<tr>";
+      for(j=0; j<m2[i].length; j++){
+         tb += "<td>";
+         tb += m2[i][j];
+         tb += "</td>";
+      }  
+   tb += "</tr>";
+}
+tb += "</table>";
+
+document.write(tb);
